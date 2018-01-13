45e6827b8064109fff3cb891a00e58b772ecf423
fabio <fabiodeoliveira07@gmail.com>
2018-01-13 18:25:03 -0200

    App Core: Criacao das views para os templates existentes no views.py

--- core/views.py
+++ core/views.py
@@ -4,9 +4,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    texts = ["Lorem ipsum dolor sit amet", "consectetur adipisicing elit"]
-    context = {
-        'title': 'django e-commerce',
-        'texts': texts
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+def product(request):
+    return render(request, 'product.html')
