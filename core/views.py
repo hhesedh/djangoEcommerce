c1d2a9b6423ec2f215aa737d5a7561dc5c7b0840
fabio <fabiodeoliveira07@gmail.com>
2018-01-13 07:35:55 -0200

    Criacao da pasta templates

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 # coding=utf-8
 
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    texts = ["Lorem ipsum dolor sit amet", "consectetur adipisicing elit"]
+    context = {
+        'title': 'django e-commerce',
+        'texts': texts
+    }
+    return render(request, 'index.html', context)
