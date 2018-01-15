bc73620130ce12866f41283977897e58d002028a
fabio <fabiodeoliveira07@gmail.com>
2018-01-15 08:19:17 -0200

    removendo $ da url produtos

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^contato/$', views.contact, name='contact'),
     url(r'^produto/$', views.product, name='product'),
-    url(r'^produtos/$', include('catalog.urls', namespace='catalog')),
+    url(r'^produtos/', include('catalog.urls', namespace='catalog')),
     url(r'^admin/', admin.site.urls),
 ]
