25b8b6b6d33ad645633c519154851fa3fae34bc2
fabio <fabiodeoliveira07@gmail.com>
2018-01-15 05:27:57 -0200

    Remocao da url produtos

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^contato/$', views.contact, name='contact'),
     url(r'^produto/$', views.product, name='product'),
-    url(r'^produtos/$', views.product_list, name='product_list'),
     url(r'^admin/', admin.site.urls),
 ]
