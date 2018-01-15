f81e08f239e4b7459394fa9e85852de8be9037ea
fabio <fabiodeoliveira07@gmail.com>
2018-01-15 05:27:15 -0200

    Criacao da view product_list em catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
+def product_list(request):
+    return render(request, 'catalog/product_list.html')
