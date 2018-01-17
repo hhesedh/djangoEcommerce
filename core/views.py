dcd1049dd632b5449b344408a9420c63d5d6cf7f
fabio <fabiodeoliveira07@gmail.com>
2018-01-17 02:38:53 -0200

    implementacao do formulario pelo metodo POST em core/views.py

--- core/views.py
+++ core/views.py
@@ -2,9 +2,19 @@
 
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+    else:
+        form = ContactForm()
+        
+    context = {
+        'form': form,
+    }
+    return render(request, 'contact.html', context)
