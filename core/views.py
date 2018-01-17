94a3b41171023f1483b089636cc3f731cc01bfb6
fabio <fabiodeoliveira07@gmail.com>
2018-01-17 04:03:23 -0200

    refatorando core/views.py

--- core/views.py
+++ core/views.py
@@ -1,9 +1,6 @@
 # coding=utf-8
 
 from django.shortcuts import render
-from django.core.mail import send_mail
-from django.conf import settings
-
 
 from .forms import ContactForm
 
@@ -13,23 +10,15 @@ def index(request):
 
 def contact(request):
     sucess = False
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            message = 'Nome: {0}\nE-mail:{1}\n{2}'.format(name, email, message)
-            send_mail(
-                'Contato do Django E-Commerce', message, settings.DEFAULT_FROM_EMAIL,
-                [settings.DEFAULT_FROM_EMAIL]
-            )
-            sucess = True
-    else:
-        form = ContactForm()
+    form = ContactForm(request.POST or None)
+
+    if form.is_valid():
+        form.send_mail()
+        sucess = True
 
     context = {
         'form': form,
         'sucess': sucess,
     }
+    
     return render(request, 'contact.html', context)
