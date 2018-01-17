bb110fc59179ed599f960928b31225f062b02469
fabio <fabiodeoliveira07@gmail.com>
2018-01-17 04:04:24 -0200

    passando o metodo send_mail para a classe ContactForm em core/forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
+from django.core.mail import send_mail
+from django.conf import settings
 
 class ContactForm(forms.Form):
 
@@ -13,3 +15,13 @@ class ContactForm(forms.Form):
 #        self.fields['name'].widget.attrs['class'] = 'form-control'
 #        self.fields['email'].widget.attrs['class'] = 'form-control'
 #        self.fields['message'].widget.attrs['class'] = 'form-control'
+
+    def send_mail(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        message = self.cleaned_data['message']
+        message = 'Nome: {0}\nE-mail:{1}\n{2}'.format(name, email, message)
+        send_mail(
+            'Contato do Django E-Commerce', message, settings.DEFAULT_FROM_EMAIL,
+            ['fabiodeoliveira07@gmail.com']
+        )
