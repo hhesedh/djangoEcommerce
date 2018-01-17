e69f0e510428516457617beac7b221e71a5ccc27
fabio <fabiodeoliveira07@gmail.com>
2018-01-17 02:39:31 -0200

    listando widget_tweaks, como um app em djangoecommerce/settings.py

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # libs
+    'widget_tweaks',
+    # apps
     'core',
     'catalog',
 ]
